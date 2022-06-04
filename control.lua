41e454bc83f030239f8c2afc5fa204beb662ff42
Tom Burrows <tomburrows13@icloud.com>
2022-06-04 19:10:12 +0100

    Fix incorrect map preset warning appearing on every load

--- control.lua
+++ control.lua
@@ -6,14 +6,17 @@ script.on_event(defines.events.on_cutscene_cancelled, function(event)
   game.get_player(event.player_index).remove_item{name = "jag-seeds", count = 1}
 end)
 
-
-script.on_nth_tick(600,
-  function()
-    if game.tick ~= 0 then
-      if game.default_map_gen_settings.property_expression_names.elevation ~= "IS_0_17-islands+continents" then
-        game.print({"x-mod.warn-nondefault-mapgen", {"map-gen-preset-name.x-default"}})
-      end
-      script.on_nth_tick(600, nil)
+local function print_warning()
+  if game.tick > 0 then
+    if game.default_map_gen_settings.property_expression_names.elevation ~= "IS_0_17-islands+continents" then
+      game.print({"x-mod.warn-nondefault-mapgen", {"map-gen-preset-name.x-default"}})
     end
+    script.on_nth_tick(300, nil)
+  end
+end
+
+script.on_init(
+  function()
+    script.on_nth_tick(300, print_warning)
   end
 )
