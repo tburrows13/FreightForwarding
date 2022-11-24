411f941d3092bad429cded2fa7c6bfb6ea7d81a2
Tom Burrows <tomburrows13@icloud.com>
2022-11-24 16:16:36 +0000

    Revert titanium additions to underwater pipes

--- data-updates.lua
+++ data-updates.lua
@@ -1,1 +1,11 @@
 require "__X-Logistics__/prototypes/inventory-sizes"
+
+-- Revert bztitanium's changes to underwater pipes
+if mods["underwater-pipes"] then
+  data.raw.technology["underwater-pipes"].prerequisites = { "concrete", "steel-processing" }
+  data.raw.recipe["underwater-pipe"].ingredients = {
+    { "pipe", 2 },
+    { "steel-plate", 5 },
+    { "concrete", 20 },
+  }
+end
