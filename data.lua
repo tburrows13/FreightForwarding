751d81fe1c01db0183730bbc3bf3481f6d6509c5
Tom Burrows <tomburrows13@icloud.com>
2022-12-04 00:14:54 +0000

    Increase radar range, decrease radar energy per sector

--- data.lua
+++ data.lua
@@ -4,3 +4,14 @@ require "__X-Logistics__/prototypes/shallow-water"
 require "__X-Logistics__/prototypes/landfill"
 require "__X-Logistics__/prototypes/crating-machine"
 require "__X-Logistics__/prototypes/map-gen-presets"
+
+local radar = data.raw["radar"]["radar"]
+radar.max_distance_of_sector_revealed = 20
+radar.energy_per_sector = "4MJ"
+--[[
+Vanilla radar:
+  max_distance_of_sector_revealed = 14
+  energy_per_sector = "10MJ"
+  max_distance_of_nearby_sector_revealed = 3
+  energy_per_nearby_scan = "250kJ"
+]]
