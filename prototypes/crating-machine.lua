020cf11a814987be2ed690002354d8dc3d76c8cd
Tom Burrows <tomburrows13@icloud.com>
2022-12-24 14:45:39 +0000

    Remove unwanted tabs

--- prototypes/crating-machine.lua
+++ prototypes/crating-machine.lua
@@ -1,4 +1,4 @@
--- Convert crating machines from 3x3 to 5x5
+-- Convert crating machines from 3x3 to 4x4
 
 SCALE = 1.33333
 
@@ -28,7 +28,7 @@ for tier = 1, 3 do
   entity.working_visualisations[1].animation.scale = 1 * SCALE
   entity.working_visualisations[1].animation.hr_version.scale = 0.5 * SCALE
   entity.working_visualisations[1].light.scale = 0.5 * SCALE
-	entity.se_allow_in_space = true
+  entity.se_allow_in_space = true
 end
 
 
@@ -38,15 +38,15 @@ item1.order = "d-a[crating-machine-1]"
 local recipe1 = data.raw.recipe["deadlock-crating-machine-1"]
 --[[ Original:
 {
-	{"assembling-machine-1",1},
-	{"electric-engine-unit",1},
-	{"stack-inserter",2},
-	{"steel-plate",20},
+  {"assembling-machine-1",1},
+  {"electric-engine-unit",1},
+  {"stack-inserter",2},
+  {"steel-plate",20},
 }]]
 recipe1.ingredients = {
-	{"engine-unit", 2},
-	{"fast-inserter", 4},
-	{"steel-plate", 5},
+  {"engine-unit", 2},
+  {"fast-inserter", 4},
+  {"steel-plate", 5},
 }
 
 local technology1 = data.raw.technology["deadlock-crating-1"]
@@ -60,16 +60,16 @@ item2.order = "d-b[crating-machine-2]"
 
 local recipe2 = data.raw.recipe["deadlock-crating-machine-2"]
 --[[ Original:
-	{"deadlock-crating-machine-1",1},
-	{"electric-engine-unit",1},
-	{"stack-inserter",2},
-	{"steel-plate",30},
+  {"deadlock-crating-machine-1",1},
+  {"electric-engine-unit",1},
+  {"stack-inserter",2},
+  {"steel-plate",30},
 ]]
 recipe2.ingredients = {
-	{"deadlock-crating-machine-1",1},
-	{"electric-engine-unit", 2},
-	{"stack-inserter", 2},
-	{"steel-plate", 20},
+  {"deadlock-crating-machine-1",1},
+  {"electric-engine-unit", 2},
+  {"stack-inserter", 2},
+  {"steel-plate", 20},
 }
 
 local technology2 = data.raw.technology["deadlock-crating-2"]
@@ -83,16 +83,16 @@ item3.order = "d-c[crating-machine-3]"
 
 local recipe3 = data.raw.recipe["deadlock-crating-machine-3"]
 --[[ Original:
-	{"deadlock-crating-machine-2",1},
-	{"electric-engine-unit",1},
-	{"stack-inserter",2},
-	{"steel-plate",50},
+  {"deadlock-crating-machine-2",1},
+  {"electric-engine-unit",1},
+  {"stack-inserter",2},
+  {"steel-plate",50},
 ]]
 recipe3.ingredients = {
-	{"deadlock-crating-machine-2", 1},
-	{"electric-engine-unit", 8},
-	{"advanced-circuit", 8},
-	{"steel-plate", 30},
+  {"deadlock-crating-machine-2", 1},
+  {"electric-engine-unit", 8},
+  {"advanced-circuit", 8},
+  {"steel-plate", 30},
 }
 
 local technology3 = data.raw.technology["deadlock-crating-3"]
