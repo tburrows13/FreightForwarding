638d4c41311b6b5d220ec3c7306c606a479c98a7
Tom Burrows <tomburrows13@icloud.com>
2023-01-13 01:28:46 +0000

    Increase boat selection priorities

--- data.lua
+++ data.lua
@@ -15,3 +15,17 @@ Vanilla radar:
   max_distance_of_nearby_sector_revealed = 3
   energy_per_nearby_scan = "250kJ"
 ]]
+
+-- Increase selection priority of cargo ships so that they can be selected over waterway
+for name, type in pairs({
+  ["ironclad"] = "car",
+  ["indep-boat"] = "car",
+  ["boat"] = "cargo-wagon",
+  ["boat_engine"] = "locomotive",
+  ["cargo_ship"] = "cargo-wagon",
+  ["oil_tanker"] = "fluid-wagon",
+  ["cargo_ship_engine"] = "locomotive"}) do
+    log(name)
+    log(type)
+  data.raw[type][name].selection_priority = 51
+end
