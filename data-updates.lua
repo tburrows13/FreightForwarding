cb448419cfaff8e06f8647dec66812a9d20b8abc
Tom Burrows <tomburrows13@icloud.com>
2023-01-13 02:14:02 +0000

    Allow Shift-Enter from cargo ships to exit onto shallow water

--- data-updates.lua
+++ data-updates.lua
@@ -9,3 +9,15 @@ if mods["underwater-pipes"] then
     { "concrete", 20 },
   }
 end
+
+-- Remove water-tile from collision check so that player can exit onto shallow water
+if mods["cargo-ships"] then
+  local collision_mask = data.raw["simple-entity-with-force"]["tile_player_test_item"].collision_mask
+  for i, mask in pairs(collision_mask) do
+    if mask == "water-tile" then
+      table.remove(collision_mask, i)
+      break
+    end
+  end
+  table.insert(collision_mask, "train-layer")
+end
