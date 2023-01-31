6e6956991c475a348522aefb1b68150d9f3c7231
RedRafe <gabri97.gt+redrafe@gmail.com>
2023-01-31 21:21:12 +0100

    Added custom Resource Autoplace module

---
+++ resource-autoplace/config.lua
@@ -0,0 +1,70 @@
+local config = {}
+
+---------------------------------------------------------------------------------------------------
+
+config.presets = {
+  ["default"] = {
+    -- -- PRESET NAME
+    preset_name = "default",
+    -- -- PARAMETERS (alphabetically)
+    additional_richness = 0,
+    base_density = 8,
+    base_quantity = 1 * 10^6,
+    base_spots_per_km2 = 2.5,
+    blob_amplitude_multiplier = 1,
+    candidate_spot_count = 21,
+    double_density_distance = 1300,
+    freq_multiplier_coefficient = 1,
+    has_starting_area_placement = nil,
+    min_radius_expression = 32,
+    minimum_richness = 0,
+    order = "d",
+    random_probability = 1,
+    random_spot_size_maximum = 2.00,
+    random_spot_size_minimum = 0.25,
+    regular_patch_fade_in_distance = 300,
+    regular_rq_factor_coefficient = 1,
+    regular_rq_factor_multiplier = 1,
+    richness_post_multiplier = 1,
+    seed = 100,
+    size_multiplier_coefficient = 1,
+    starting_amount = 40000,
+    starting_resource_placement_radius = 120,
+    starting_rq_factor_coefficient = 1,
+    starting_rq_factor_multiplier = 1,
+  },
+  ["x-logistic"] = {
+    -- -- PRESET NAME
+    preset_name = "x-logistic",
+    -- -- PARAMETERS (alphabetically)
+    additional_richness = 0,
+    base_density = 8,
+    base_quantity = 1 * 10^6,
+    base_spots_per_km2 = 2.5,
+    blob_amplitude_multiplier = 1,
+    candidate_spot_count = 21,
+    double_density_distance = 1300,
+    freq_multiplier_coefficient = 1,
+    has_starting_area_placement = nil,
+    min_radius_expression = 32,
+    minimum_richness = 0,
+    order = "d",
+    random_probability = 1,
+    random_spot_size_maximum = 2.00,
+    random_spot_size_minimum = 0.25,
+    regular_patch_fade_in_distance = 300,
+    regular_rq_factor_coefficient = 1,
+    regular_rq_factor_multiplier = 1,
+    richness_post_multiplier = 1,
+    seed = 100,
+    size_multiplier_coefficient = 1,
+    starting_amount = 40000,
+    starting_resource_placement_radius = 120,
+    starting_rq_factor_coefficient = 1,
+    starting_rq_factor_multiplier = 1,
+  },
+}
+
+---------------------------------------------------------------------------------------------------
+
+return config
