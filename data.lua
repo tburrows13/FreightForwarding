6e1ca3dad8715c4dc3f2724b525d01b19a648f54
Tom Burrows <tomburrows13@icloud.com>
2023-02-01 23:24:36 +0000

    Prevent rail bridges on deep water, change all vehicle selection priorities to 51

--- data.lua
+++ data.lua
@@ -2,6 +2,7 @@ require "__X-Logistics__/prototypes/greenhouse"
 require "__X-Logistics__/prototypes/recipe"
 require "__X-Logistics__/prototypes/shallow-water"
 require "__X-Logistics__/prototypes/landfill"
+require "__X-Logistics__/prototypes/rail-bridge"
 require "__X-Logistics__/prototypes/crating-machine"
 require "__X-Logistics__/prototypes/map-gen-presets"
 
@@ -15,17 +16,3 @@ Vanilla radar:
   max_distance_of_nearby_sector_revealed = 3
   energy_per_nearby_scan = "250kJ"
 ]]
-
--- Increase selection priority of cargo ships so that they can be selected over waterway
-for name, type in pairs({
-  ["ironclad"] = "car",
-  ["indep-boat"] = "car",
-  ["boat"] = "cargo-wagon",
-  ["boat_engine"] = "locomotive",
-  ["cargo_ship"] = "cargo-wagon",
-  ["oil_tanker"] = "fluid-wagon",
-  ["cargo_ship_engine"] = "locomotive"}) do
-  if data.raw[type][name] then
-    data.raw[type][name].selection_priority = 51
-  end
-end
---
+++ prototypes/rail-bridge.lua
@@ -0,0 +1,31 @@
+-- Modifications to beautiful_bridge_railway_Cargoships
+
+local collision_mask_util = require "__core__.lualib.collision-mask-util"
+
+-- Prevent bridges being placed on deep water
+local deep_water_mask = collision_mask_util.get_first_unused_layer()
+table.insert(data.raw.tile["deepwater"].collision_mask, deep_water_mask)
+table.insert(data.raw.tile["deepwater-green"].collision_mask, deep_water_mask)
+
+for _, bridge_type in pairs({"wood", "iron", "brick"}) do
+  for _, rail_type in pairs({"straight-rail", "curved-rail"}) do
+    local prototype = data.raw[rail_type]["bbr-" .. rail_type .. "-" .. bridge_type]
+    if prototype then
+      local collision_mask = collision_mask_util.get_mask(prototype)
+      collision_mask_util.add_layer(collision_mask, deep_water_mask)
+      prototype.collision_mask = collision_mask
+      prototype.localised_description = {"item-description.bbr-rail-common"}
+    end
+  end
+end
+
+
+-- Increase selection priority of vehicles so that they can be selected over bridges
+for _, type in pairs({"car", "locomotive", "cargo-wagon", "fluid-wagon", "artillery-wagon", "spider-vehicle"}) do
+  for _, prototype in pairs(data.raw[type]) do
+    log(prototype.name)
+    if not prototype.selection_priority or prototype.selection_priority == 50 then
+      prototype.selection_priority = 51
+    end
+  end
+end
