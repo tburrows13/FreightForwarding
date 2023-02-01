ca30f3ae5dc66585493925d8e6a522373ff3c00d
Tom Burrows <tomburrows13@icloud.com>
2023-02-01 22:34:40 +0000

    Tweak lead autoplace, add titanium autoplace

--- data-updates.lua
+++ data-updates.lua
@@ -33,10 +33,27 @@ if mods["bzlead"] then
     order = "b-z",
     base_density = 6,
     base_spots_per_km2 = 1,
-    has_starting_area_placement = nil,
+    has_starting_area_placement = false,
     regular_rq_factor_multiplier = 1.2,
     starting_rq_factor_multiplier = 1.7,
-    starting_resource_placement_radius = 100,
-    regular_patch_fade_in_distance = 300,
+    starting_resource_placement_radius = 200,
+    regular_patch_fade_in_distance = 200,
+  }
+end
+
+-- BZ Lead compatibility - override resource autoplace
+if mods["bztitanium"] then
+  -- import custom Resource Autoplace
+  local resource_autoplace = require("resource-autoplace/resource-autoplace")
+
+  -- override BZ's autoplace, but keeping same values for richness
+  data.raw.resource["titanium-ore"].autoplace = resource_autoplace.resource_autoplace_settings{
+    name = "titanium-ore",
+    order = "b-z",
+    base_density = 3,
+    has_starting_area_placement = false,
+    regular_rq_factor_multiplier = 0.8,
+    starting_resource_placement_radius = 500,
+    regular_patch_fade_in_distance = 500,
   }
 end
