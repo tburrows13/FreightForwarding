d2332d66bd4f1ed38c2a03f3b762e65314653c2e
Tom Burrows <tomburrows13@icloud.com>
2023-02-04 22:12:06 +0000

    Buff radars even more

--- data.lua
+++ data.lua
@@ -8,11 +8,14 @@ require "__X-Logistics__/prototypes/map-gen"
 require "__X-Logistics__/prototypes/map-gen-presets"
 
 local radar = data.raw["radar"]["radar"]
-radar.max_distance_of_sector_revealed = 20
-radar.energy_per_sector = "4MJ"
+radar.max_distance_of_sector_revealed = 22  -- 45x45 chunk square
+radar.energy_per_sector = "3MJ"
+radar.max_distance_of_nearby_sector_revealed = 4
+radar.energy_per_nearby_scan = "150kJ"
+
 --[[
 Vanilla radar:
-  max_distance_of_sector_revealed = 14
+  max_distance_of_sector_revealed = 14  -- 29x29 chunk square
   energy_per_sector = "10MJ"
   max_distance_of_nearby_sector_revealed = 3
   energy_per_nearby_scan = "250kJ"
