8d410fef33dac59bc1bda232be9ef55fd3fbe21b
Tom Burrows <tomburrows13@icloud.com>
2023-02-06 02:22:30 +0000

    Tech tree prerequisites tweak

--- prototypes/recipe.lua
+++ prototypes/recipe.lua
@@ -1,9 +1,15 @@
 local util = require "__X-Logistics__/prototypes/data-util"
 local bzutil = require("__bzlead__/data-util")
 
+util.remove_prerequisite("railway", "engine")
+util.add_prerequisite("railway", "deadlock-crating-1")
+
 -- Rails are now used in transport science, so swap them out for uranium
 bzutil.remove_ingredient("production-science-pack", "rail")
 bzutil.add_ingredient("production-science-pack", "uranium-238", 2)
+
+util.remove_prerequisite("production-science-pack", "railway")
+util.add_prerequisite("production-science-pack", "uranium-processing")
 
 -- Add lead to boats
 bzutil.replace_ingredient("boat", "iron-gear-wheel", "lead-plate")
