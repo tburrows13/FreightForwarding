ed917f6cbb09d13e757c709a61295ace0252e05a
Tom Burrows <tomburrows13@icloud.com>
2023-02-27 01:48:27 +0000

    Fix gun turret still requiring lead

--- prototypes/recipe-updates.lua
+++ prototypes/recipe-updates.lua
@@ -3,7 +3,8 @@ local bzutil = require "__bzlead__/data-util"
 
 -- Revert bzlead's changes to early game things that need to be built before lead is accessible
 bzutil.replace_ingredient("pipe", "lead-plate", "iron-plate")
-bzutil.replace_ingredient("gun-turret", "lead-plate", "iron-plate")
+bzutil.remove_ingredient("gun-turret", "lead-plate")
+bzutil.set_ingredient("gun-turret", "iron-plate", 20)
 bzutil.replace_ingredient("firearm-magazine", "lead-plate", "iron-plate")
 bzutil.remove_ingredient("piercing-rounds-magazine", "firearm-magazine")
 bzutil.add_ingredient("piercing-rounds-magazine", "lead-plate", 4)
