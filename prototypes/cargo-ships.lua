525a40e42a9e8b99537bef1c6f3a77bce522cc3e
Tom Burrows <tomburrows13@icloud.com>
2023-03-01 02:59:53 +0000

    Make boat slightly faster

--- prototypes/cargo-ships.lua
+++ prototypes/cargo-ships.lua
@@ -11,3 +11,6 @@ table.insert(data.raw["electric-pole"]["floating-electric-pole"].flags, "hidden"
 table.insert(data.raw["item"]["floating-electric-pole"].flags, "hidden")
 data.raw["recipe"]["floating-electric-pole"].hidden = true
 data.raw["technology"]["oversea-energy-distribution"].hidden = true
+
+-- Speed up small boat
+data.raw["car"]["indep-boat"].friction = data.raw["car"]["indep-boat"].friction * 0.75
