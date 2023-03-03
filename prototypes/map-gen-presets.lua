097358d404b22f939836f3b4dc0e64ebb6c4a17d
Tom Burrows <tomburrows13@icloud.com>
2023-03-03 04:09:58 +0000

    Decrease default enemy difficulty

--- prototypes/map-gen-presets.lua
+++ prototypes/map-gen-presets.lua
@@ -12,14 +12,18 @@ presets["x-default"] = {
       ["coal"] = ore_default,
       ["uranium-ore"] = ore_default,
       ["crude-oil"] = ore_default,]]
+      ["enemy-base"] = { frequency = 0.5, size = 0.5 },
     },
     terrain_segmentation = 1,  -- Inverse of Water Scale
     water = 1,  -- Water Coverage
-    property_expression_names = {  -- From IslandStart mod
+    property_expression_names = {
       elevation = "x-continents",
     },
   },
   advanced_settings = {
+    enemy_evolution = {
+      time_factor = 0.000001  -- Default is 0.000004
+    },
     difficulty_settings = {
       research_queue_setting = "always"
     }
