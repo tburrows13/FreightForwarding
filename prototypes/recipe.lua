a8d5a7e542ba5b078ee8207d257c4dda5c14b8df
Tom Burrows <tomburrows13@icloud.com>
2023-03-10 20:43:07 +0000

    Fix loading without FF Pack

--- prototypes/recipe.lua
+++ prototypes/recipe.lua
@@ -33,21 +33,27 @@ bzutil.replace_ingredient("lex-flying-gunship", "steel-plate", "titanium-plate")
 bzutil.replace_ingredient("lex-flying-cargo", "steel-plate", "titanium-plate")
 bzutil.replace_ingredient("lex-flying-heavyship", "steel-plate", "titanium-plate")
 
-bzutil.replace_ingredient("big-mining-drill", "steel-plate", "titanium-plate")
-util.add_prerequisite("big-mining-drill", "titanium-processing")
+if mods["big-mining-drill"] then
+  bzutil.replace_ingredient("big-mining-drill", "steel-plate", "titanium-plate")
+  util.add_prerequisite("big-mining-drill", "titanium-processing")
+end
 
 bzutil.add_ingredient("rocket-control-unit", "ff-cobalt-ingot", 1)
 util.add_prerequisite("rocket-control-unit", "ff-cobalt-processing")
 
-bzutil.add_ingredient("chcs-heliostat-mirror", "ff-cobalt-ingot", 1)
-bzutil.add_ingredient("chcs-solar-power-tower", "ff-cobalt-ingot", 50)
-bzutil.add_ingredient("chcs-solar-laser-tower", "ff-cobalt-ingot", 50)
-util.add_prerequisite("chcs-concentrated-solar-energy", "ff-cobalt-processing")
-util.add_research_ingredient("chcs-concentrated-solar-energy", "utility-science-pack")
-util.add_research_ingredient("chcs-weaponized-solar-energy", "utility-science-pack")
+if mods["ch-concentrated-solar"] then
+  bzutil.add_ingredient("chcs-heliostat-mirror", "ff-cobalt-ingot", 1)
+  bzutil.add_ingredient("chcs-solar-power-tower", "ff-cobalt-ingot", 50)
+  bzutil.add_ingredient("chcs-solar-laser-tower", "ff-cobalt-ingot", 50)
+  util.add_prerequisite("chcs-concentrated-solar-energy", "ff-cobalt-processing")
+  util.add_research_ingredient("chcs-concentrated-solar-energy", "utility-science-pack")
+  util.add_research_ingredient("chcs-weaponized-solar-energy", "utility-science-pack")
+end
 
 bzutil.replace_ingredient("satellite", "accumulator", "ff-battery-pack")
 util.add_prerequisite("space-science-pack", "ff-battery-pack")
 
-bzutil.add_ingredient("plutonium-atomic-bomb", "ff-titansteel-plate", 1)
-util.add_prerequisite("plutonium-atomic-bomb", "ff-titansteel-processing")
+if mods["PlutoniumEnergy"] then
+  bzutil.add_ingredient("plutonium-atomic-bomb", "ff-titansteel-plate", 1)
+  util.add_prerequisite("plutonium-atomic-bomb", "ff-titansteel-processing")
+end
