2f998beb48f0152013f537b096b6fca09ad5a802
RedRafe <gabri97.gt+redrafe@gmail.com>
2023-03-28 16:41:01 +0200

    add conditional enabling of Transport Drones

--- compatibility/transport-drones/data-final-fixes.lua
+++ compatibility/transport-drones/data-final-fixes.lua
@@ -1,5 +1,6 @@
--- Remove all crate request recipes so that crates cannot be transported by transport drones
+if not mods["Transport_Drones"] then return end
 
+-- Remove all crate request recipes so that crates cannot be transported by transport drones
 for name, recipe in pairs(data.raw.recipe) do
   if recipe.category and recipe.category == "transport-drone-request" 
   and recipe.subgroup and recipe.subgroup:sub(1, #"ic-load-") == "ic-load-" then
