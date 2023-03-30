aedebc9a4eb160a426720f16aefea4fa2500ecdc
Tom Burrows <tomburrows13@icloud.com>
2023-03-30 01:38:48 +0100

    Rocket-Silo Construction compatibility

---
+++ compatibility/rocket-silo-construction/data-updates.lua
@@ -0,0 +1,4 @@
+if not mods["Rocket-Silo-Construction"] then return end
+
+local bzutil = require "__bzlead__/data-util"
+bzutil.replace_ingredient("rsc-construction-stage5", "steel-plate", "ff-titansteel-plate")
