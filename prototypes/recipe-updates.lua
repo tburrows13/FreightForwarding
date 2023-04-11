d823d77d5d55f0a6c5942fd84e19f37186861ef6
Tom Burrows <tomburrows13@icloud.com>
2023-04-12 00:44:49 +0100

    Remove titanium from hovercraft, add to missilecraft

--- prototypes/recipe-updates.lua
+++ prototypes/recipe-updates.lua
@@ -8,3 +8,10 @@ bzutil.set_ingredient("gun-turret", "iron-plate", 20)
 bzutil.replace_ingredient("firearm-magazine", "lead-plate", "iron-plate")
 bzutil.remove_ingredient("piercing-rounds-magazine", "firearm-magazine")
 bzutil.add_ingredient("piercing-rounds-magazine", "lead-plate", 4)
+
+-- Revert bztitanium's changes to hovercrafts
+bzutil.replace_ingredient("hcraft-recipe", "titanium-plate", "steel-plate")
+bzutil.remove_prerequisite("hcraft-tech", "titanium-processing")
+
+bzutil.add_ingredient("mcraft-recipe", "titanium-plate", 20)
+bzutil.add_prerequisite("mcraft-tech", "titanium-processing")
