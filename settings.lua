63852f87cbadff3f73a65e0c8f5cc9e2bd1756cb
Tom Burrows <tomburrows13@icloud.com>
2023-04-12 00:48:27 +0100

    Add boblogistics incompatibility

--- settings.lua
+++ settings.lua
@@ -53,7 +53,5 @@ force_setting("bool", "sp-remove-military-requirement", true)
 -- Ensure missile hovercraft exists
 force_setting("bool", "enable-mcraft", true)
 
-force_setting("bool", "bobmods-logistics-trains", false)
-
 require "__FreightForwarding__/compatibility/beautiful-bridge-railway/settings"
 require "__FreightForwarding__/compatibility/krastorio2/settings"
