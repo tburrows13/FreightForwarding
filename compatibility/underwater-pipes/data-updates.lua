44f1c07cbaa2a41f4b6d30e643668e3041e112fc
Tom Burrows <tomburrows13@icloud.com>
2023-05-31 03:31:29 +0100

    Compatibility for when underwater-pipes is still enabled

--- compatibility/underwater-pipes/data-updates.lua
+++ compatibility/underwater-pipes/data-updates.lua
@@ -2,11 +2,19 @@ if not mods["underwater-pipes"] then return end
 
 local util = require "__FreightForwarding__.prototypes.data-util"
 
+-- Main underwater pipe is now provided by FF, so hide it
+
+table.insert(data.raw.pipe["underwater-pipe"].flags, "hidden")
+table.insert(data.raw.pipe["underwater-pipe-placer"].flags, "hidden")
+data.raw.item["underwater-pipe"].flags = {"hidden"}
+data.raw.recipe["underwater-pipe"].hidden = true
+
+data.raw.recipe["underwater-pipe-to-ground"].ingredients = {
+  {"ff-underwater-pipe", 20},
+}
+
+table.remove(data.raw.technology["underwater-pipes"].effects, 1)  -- Remove underwater pipe
+
 -- Revert bztitanium's changes to underwater pipes
 util.remove_prerequisite("underwater-pipes", "titanium-processing")
 util.add_prerequisite("underwater-pipes", "automated_water_transport")
-data.raw.recipe["underwater-pipe"].ingredients = {
-  { "pipe", 2 },
-  { "steel-plate", 5 },
-  { "concrete", 20 },
-}
