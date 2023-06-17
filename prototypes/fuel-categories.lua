5abebd178a862acbb0be15a11927cdfa3be2f99e
Tom Burrows <tomburrows13@icloud.com>
2023-06-17 04:29:42 +0100

    Fix Lex's Aircraft not accepting advanced fuels

--- prototypes/fuel-categories.lua
+++ prototypes/fuel-categories.lua
@@ -6,9 +6,12 @@ for _, type in pairs(entity_types) do
   for name, prototype in pairs(data.raw[type]) do
     if not prototype.ff_no_advanced_fuel then
       local energy_source = prototype.burner or prototype.energy_source
-      if energy_source.fuel_categories and bzutil.contains(energy_source.fuel_categories, "chemical") then
-        table.insert(energy_source.fuel_categories, "advanced-chemical")
-      elseif energy_source.fuel_category == "chemical" then
+      if energy_source.fuel_categories then
+        if bzutil.contains(energy_source.fuel_categories, "chemical") then
+          table.insert(energy_source.fuel_categories, "advanced-chemical")
+        end
+      elseif energy_source.fuel_category == "chemical" or not energy_source.fuel_category then
+        -- Default fuel_category is "chemical"
         energy_source.fuel_categories = {"chemical", "advanced-chemical"}
         energy_source.fuel_category = nil
       end
