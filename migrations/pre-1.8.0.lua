65ebb306e7462efc708893063d473d0b36461261
Tom Burrows <tomburrows13@icloud.com>
2023-11-08 21:45:07 +0000

    Fix 1.8.0 migration with MSI

--- migrations/pre-1.8.0.lua
+++ migrations/pre-1.8.0.lua
@@ -1,3 +1,5 @@
+if script.active_mods["Mining-Space-Industries-II"] or script.active_mods["LunarLandings"] or script.active_mods["space-exploration"] then return end
+
 for _, surface in pairs(game.surfaces) do
-    surface.regenerate_entity("ff-rocket-silo-hole")
-  end
+  surface.regenerate_entity("ff-rocket-silo-hole")
+end
