b4e6216f4d344bf05c8b0dfd14625025dbba42cf
RedRafe <gabri97.gt+redrafe@gmail.com>
2023-12-02 19:23:11 +0100

    K2: edit conditional lead/mmo setting

--- compatibility/krastorio2/settings.lua
+++ compatibility/krastorio2/settings.lua
@@ -4,13 +4,14 @@ if data.raw["string-setting"]["kr-stack-size"] then
   data.raw["string-setting"]["kr-stack-size"].hidden = true
 end
 
-
-data:extend{
-  {
-    type = "bool-setting",
-    name = "ff-k2-no-lead-ammo",
-    order = "c",
-    setting_type = "startup",
-    default_value = true
-  },
-}
+if mods["Krastorio2"] then 
+  data:extend{
+    {
+      type = "bool-setting",
+      name = "ff-k2-no-lead-ammo",
+      order = "c",
+      setting_type = "startup",
+      default_value = true
+    },
+  }
+end
